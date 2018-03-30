e48fa018cbfe30e6836045a8de245d98f22be609
Lejla Bajgoric <lbajgoric1@etf.unsa.ba>
2018-03-30 22:03:56 +0200

    Modifikovana metoda u CourseService

--- CourseMicroservice/src/main/java/com/example/demo/Services/CourseService.java
+++ CourseMicroservice/src/main/java/com/example/demo/Services/CourseService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import com.example.demo.Interfaces.ICourseService;
 import com.example.demo.Models.Course;
 import com.example.demo.Models.User;
+import com.example.demo.Repositories.CourseRepository;
+import com.example.demo.Repositories.UserRepository;
 
 @Service("courseService")
 @Transactional
@@ -21,19 +23,27 @@ public class CourseService implements ICourseService {
 	private static final AtomicLong counter = new AtomicLong();
 	private static List<Course> courses;
 	
+	
 	static { 
 		courses = populateDummyCourses();
 	}
 	
 	private static List<Course> populateDummyCourses() {
 		List<Course> courses = new ArrayList<Course>();
+				
+		User user1 = new User("Imenko","Prezimenko");
+		User user2 = new User("Selma","Glavić");
+		User user3 = new User("Admin","Admin");
+		User user4 = new User("Almedin","Velija");
+		User user5 = new User("Lejla","Bajgorić");
+		User user6 = new User("Amer","Kodžaga");
+		
 		Course course1 = new Course("KURS1", "KOD", 5.0, "OPIS");
 		Course course2 = new Course("KURS2", "KOD2", 6.5, "OPIS2");
-		courses.add(course1);
-		courses.add(course2);
-		
-		/*User user1 = new User("Imenko","Prezimenko");
-		User user2 = new User("Selma","Glavić");
+		Course course3 = new Course("KURS3", "KOD3", 9.0, "OPIS3");
+		Course course4 = new Course("KURS4", "KOD4", 6.0, "OPIS4");
+		Course course5 = new Course("KURS5", "KOD5", 8.0, "OPIS5");
+		Course course6 = new Course("KURS6", "KOD6", 6.0, "OPIS6");
 		
 		User user7 = new User("NN","NN",new HashSet<Course>(){{
             add(course1);
@@ -45,17 +55,24 @@ public class CourseService implements ICourseService {
             add(course2);
         }});
 		
-		user2.setCourses(new HashSet<Course>(){{
-            add(course1);
-            add(course2);
+		user4.setCourses(new HashSet<Course>(){{
+            add(course5);
+            add(course6);
         }});
 		
-		List<User> users = new ArrayList<User>();
+		Set users = new HashSet<User>();
 		users.add(user1);
 		users.add(user2);
-		users.add(user7);
-		course1.setUsers((Set<User>) users);
-		course2.setUsers((Set<User>) users);*/
+		users.add(user3);
+		
+		//course1.setUsers(users);
+		
+		courses.add(course1);
+		courses.add(course2);
+		courses.add(course3);
+		courses.add(course4);
+		courses.add(course5);
+		courses.add(course6);
 		
 		return courses;
 	}
@@ -104,12 +121,15 @@ public class CourseService implements ICourseService {
 		for (Iterator<Course> iterator = courses.iterator(); iterator.hasNext(); ) {
 			Course c = iterator.next();
 			if (c.getId() == course.getId()) {
-				for (Iterator<User> i = c.getUsers().iterator(); i.hasNext(); ) {
+				if (c.getUsers().contains(user)) {
+					c.getUsers().remove(user);
+				}
+				/*for (Iterator<User> i = c.getUsers().iterator(); i.hasNext(); ) {
 					User u = i.next();
 					if (u.getCourses().contains(c)) {
 						i.remove();
 					}
-				}
+				}*/
 			}
 		}
 		
